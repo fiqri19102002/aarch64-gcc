38665a0be05cca47043129cc45d93d87bef98442
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-03-06 08:40:29 +0700

    Update to https://github.com/gcc-mirror/gcc/commit/ceae9533826aabaf4c78d173c60e3bedeffc6955, 06032021 build.
    GCC VERSION: aarch64-linux-gnu-gcc (GCC) 11.0.1 20210306 (experimental)
    GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/ceae9533826aabaf4c78d173c60e3bedeffc6955
    BUILDER COMMIT: fee7e9add8e191ff9bf92bfeaf7fd34b2c5be0fe

--- aarch64-linux-gnu/include/c++/11.0.1/aarch64-linux-gnu/bits/c++config.h
+++ aarch64-linux-gnu/include/c++/11.0.1/aarch64-linux-gnu/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 11
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20210305
+#define __GLIBCXX__ 20210306
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
